76c25ef126669cae99ed72bbabfde70cc049467b
shraddha-bijotkar <63249066+shraddha-bijotkar@users.noreply.github.com>
2023-08-13 21:08:54 +0530

    CSS edits

--- src/app/components/home/projects/projects.component.ts
+++ src/app/components/home/projects/projects.component.ts
@@ -5,21 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
 })
-export class ProjectsComponent {
-  projects = [
-    {
-      id: 0,
-      title: 'Personal website',
-      content: 'Angular 10, Bootstrap, Html, CSS',
-      date: '2021',
-      icon: 'https://image.flaticon.com/icons/svg/214/214335.svg',
-    },
-    {
-      id: 1,
-      title: 'Interview Bot',
-      content: 'Angular 8, Node.js, MySQL, Dialogflow',
-      date: '2020 - 2021',
-      icon: 'https://image.flaticon.com/icons/svg/1006/1006517.svg',
-    },
-  ];
-}
+export class ProjectsComponent {}
